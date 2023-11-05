3b62a21693309e4054201234c629c8027706d954
carlos-adir <carlos.adir.leite@gmail.com>
2023-11-05 12:12:48 +0100

    Set initial configurations to publish package

--- src/compmec/section/__init__.py
+++ src/compmec/section/__init__.py
@@ -3,7 +3,7 @@ This module analyse sections of beams by using the boundary element method
 It uses mainly curves as boundary to compute the elements.
 """
 
-__version__ = "0.0.1"
+__version__ = "0.2.0"
 
 if __name__ == "__main__":
     pass
