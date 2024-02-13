b4388a36629ee108fb0b2062ad9a30a16f503d42
Carlos Adir <carlos.adir.leite@gmail.com>
2024-02-13 19:43:13 +0000

    Correct isort and black format

--- tests/test_material.py
+++ tests/test_material.py
@@ -1,7 +1,9 @@
+import os
+
 import pytest
 
 from compmec.section import material
-import os
+
 
 @pytest.mark.order(1)
 @pytest.mark.dependency()
@@ -72,11 +74,10 @@ class TestFromJson:
     @pytest.mark.timeout(1)
     @pytest.mark.dependency(depends=["TestFromJson::test_begin"])
     def test_main(self):
-        folder = os.getcwd()
         json_filepath = "tests/json/steel_square.json"
         mats = material.Isotropic.from_json(json_filepath)
         steel = mats["steel"]
-        assert steel.young_modulus == 210e+3
+        assert steel.young_modulus == 210e3
         assert steel.poissons_ratio == 0.3
 
     @pytest.mark.order(1)
