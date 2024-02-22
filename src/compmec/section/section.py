0afb3ea43578c36eeb28a084a81610237da11ec8
carlos-adir <carlos.adir.leite@gmail.com>
2024-02-22 20:32:45 +0100

    refactor: remove futher implement objects
    remove objects that will be implemented only in future versions, like ChargedField and so on

--- src/compmec/section/section.py
+++ src/compmec/section/section.py
@@ -6,13 +6,11 @@ from typing import Dict, Tuple, Union
 import numpy as np
 from compmec.shape.shape import DefinedShape, IntegrateShape
 
-from compmec import nurbs
-
 from . import dataio
 from .material import Material
 
 
-class BaseSection(object):
+class BaseSection:
     @classmethod
     def from_json(cls, filepath: str) -> Dict[str, BaseSection]:
         raise NotImplementedError
@@ -122,14 +120,10 @@ class Section(BaseSection):
     For composite sections, use CompositeSection class
     """
 
-    AUTO_SOLVE = True
-
-    def __init__(self, shapes: Tuple[DefinedShape], materials: Tuple[Material]):
+    def __init__(
+        self, shapes: Tuple[DefinedShape], materials: Tuple[Material]
+    ):
         super().__init__(shapes, materials)
-        self.__area = None
-        self.__first = None
-        self.__second = None
-        self.__charged_field = ChargedField(self)
 
     def area(self) -> float:
         """Gives the cross-section area
@@ -175,8 +169,12 @@ class Section(BaseSection):
 
         """
         if self.__first is None:
-            Qx = sum(IntegrateShape.polynomial(shape, 0, 1) for shape in self.shapes)
-            Qy = sum(IntegrateShape.polynomial(shape, 1, 0) for shape in self.shapes)
+            Qx = sum(
+                IntegrateShape.polynomial(shape, 0, 1) for shape in self.shapes
+            )
+            Qy = sum(
+                IntegrateShape.polynomial(shape, 1, 0) for shape in self.shapes
+            )
             self.__first = (Qx, Qy)
         return tuple(self.__first)
 
@@ -207,9 +205,15 @@ class Section(BaseSection):
 
         """
         if self.__second is None:
-            Ixx = sum(IntegrateShape.polynomial(shape, 0, 2) for shape in self.shapes)
-            Ixy = sum(IntegrateShape.polynomial(shape, 1, 1) for shape in self.shapes)
-            Iyy = sum(IntegrateShape.polynomial(shape, 2, 0) for shape in self.shapes)
+            Ixx = sum(
+                IntegrateShape.polynomial(shape, 0, 2) for shape in self.shapes
+            )
+            Ixy = sum(
+                IntegrateShape.polynomial(shape, 1, 1) for shape in self.shapes
+            )
+            Iyy = sum(
+                IntegrateShape.polynomial(shape, 2, 0) for shape in self.shapes
+            )
             self.__second = (Ixx, Ixy, Iyy)
         area = self.area()
         Ixx, Ixy, Iyy = self.__second
@@ -337,198 +341,3 @@ class Section(BaseSection):
         area = self.area()
         Ixx, _, Iyy = self.second_moment()
         return np.sqrt(Iyy / area), np.sqrt(Ixx / area)
-
-    def warping_constant(self) -> float:
-        """Gives the warping constant"""
-        raise NotImplementedError
-
-    def monosymmetry_constants(self) -> Tuple[float]:
-        raise NotImplementedError
-
-    def solve(self, meshsize: float = None, degree: int = 1):
-        raise NotImplementedError
-
-    def warping(self) -> WarpingField:
-        if self.__warping is None:
-            if not self.AUTO_SOLVE:
-                msg = "You must solve the system before getting warping"
-                raise RuntimeError(msg)
-            self.solve()
-        return self.__warping
-
-    def charged_field(self) -> ChargedField:
-        return self.__charged_field
-
-
-class Field(object):
-    def __init__(self, section: Section):
-        if not isinstance(section, Section):
-            msg = f"section is not a Section, but {type(section)}"
-            raise TypeError(msg)
-        self.__section = section
-
-    @property
-    def section(self) -> Section:
-        return self.__section
-
-
-class WarpingField(Field):
-    def __init__(self, section: Section):
-        super().__init__(section)
-        self.__values = None
-
-    @property
-    def values(self):
-        return self.__values
-
-    @values.setter
-    def values(self, vals: Tuple[Tuple[float]]):
-        self.__values = np.array(vals, dtype="float64")
-
-
-class ChargedField(Field):
-    def __init__(self, section: Section):
-        super().__init__(section)
-        self.__charges = np.zeros(6, dtype="float64")
-
-    @property
-    def forces(self) -> Tuple[float]:
-        return self.__charges[:3]
-
-    @property
-    def moments(self) -> Tuple[float]:
-        return self.__charges[3:]
-
-    @forces.setter
-    def forces(self, forces: Tuple[float]):
-        forces = np.array(forces, dtype="float64")
-        if forces.shape != (3,):
-            msg = "Forces must be a vector of lenght 3"
-            raise ValueError(msg)
-        if forces[0] != 0 or forces[1] != 0:
-            msg = "Shear is not yet supported"
-            raise NotImplementedError(msg)
-        self.__charges[:3] = forces
-
-    @moments.setter
-    def moments(self, moments: Tuple[float]):
-        moments = np.array(moments, dtype="float64")
-        if moments.shape != (3,):
-            msg = "Moments must be a vector of lenght 3"
-            raise ValueError(msg)
-        if moments[2] != 0:
-            msg = "Torsion is not yet supported"
-            raise NotImplementedError(msg)
-        self.__charges[3:] = moments
-
-    def eval(self, points: Tuple[Tuple[float]]) -> Tuple[Tuple[float]]:
-        """
-        Receives a matrix P (list of points) of shape (n, 2)
-        and returns a matrix M of shape (n, 8) that contains
-        the strain and stress
-
-        M_i = [s_zz, s_xz, s_yz, e_xx, e_yy, e_zz, e_xz, e_yz]
-
-        """
-        if not isinstance(points, np.ndarray):
-            points = np.array(points, dtype="float64")
-        if len(self.section.shapes) != 1:
-            raise NotImplementedError
-        values = np.zeros((len(points), 8), dtype="float64")
-
-        contains = self.section.shapes[0].contains_point
-        mask = tuple(contains(point, True) for point in points)
-        mask = np.array(mask, dtype="bool")
-
-        if np.any(mask):
-            values[mask, :3] = self.__eval_stresses(points[mask])
-        material = self.section.materials[0]
-        values[:, 5] = values[:, 0] / material.young_modulus  # e_zz
-        values[:, 3] = -material.poissons_ratio * values[:, 1]  # e_xx
-        values[:, 4] = values[:, 3]  # e_yy
-        values[:, 6] = 0.5 * values[:, 1] / material.shear_modulus  # e_xz
-        values[:, 7] = 0.5 * values[:, 2] / material.shear_modulus  # e_yz
-        return values
-
-    def __eval_stresses(self, points: np.ndarray) -> np.ndarray:
-        """
-        Returns the strain values of
-        [szz, sxz, syz]
-
-        We suppose that all points are inside the section.
-        No verification are made here
-        """
-        assert len(self.section.shapes) == 1  # For now
-        stresses = np.zeros((len(points), 3), dtype="float64")
-        stresses[:, 0] = self.__axial_stresses(points)
-
-        contains = self.section.shapes[0].contains_point
-        mask = tuple(contains(point, False) for point in points)
-        mask = np.array(mask, dtype="bool")
-
-        if np.any(mask):
-            inside_points = points[mask]
-            inside_shear_stresses = self.__inside_shear_stress(inside_points)
-            stresses[mask, 1:] = inside_shear_stresses
-
-        if not np.all(mask):
-            bound_points = points[~mask]
-            bound_shear_stresses = self.__boundary_shear_stress(bound_points)
-            stresses[~mask, 1:] = bound_shear_stresses
-
-        return stresses
-
-    def __axial_stresses(self, points: np.ndarray) -> Tuple[float]:
-        """
-        Computes the values of s_zz due to normal force and bending moments
-        """
-        values = np.zeros(len(points), dtype="float64")
-
-        _, _, forcez = self.forces
-        momentx, momenty, _ = self.moments
-
-        if forcez:
-            values.fill(forcez / self.section.area())
-        if momentx or momenty:
-            center = self.section.bending_center()
-            Ixx, Ixy, Iyy = self.section.second_moment(center)
-            xvals, yvals = np.transpose(points)
-            xvals -= center[0]
-            yvals -= center[1]
-
-            values += (Iyy * momentx + Ixy * momenty) * yvals
-            values -= (Ixy * momentx + Ixx * momenty) * xvals
-            values /= Ixx * Iyy - Ixy**2
-        return values
-
-    def __boundary_shear_stress(
-        self, params: Tuple[float], curve: nurbs.Curve
-    ) -> np.ndarray:
-        """
-        Computes the values of (s_xz, s_yz), the shear stresses
-
-        It's caused by two phenomenums: torsion and shear loading
-
-
-        """
-        values = np.zeros((len(params), 2), dtype="float64")
-        if self.forces[0] or self.forces[1]:  # Shear force
-            raise NotImplementedError
-        if self.moments[2]:  # Torsion moment
-            raise NotImplementedError
-        return values
-
-    def __inside_shear_stress(self, points: np.ndarray) -> np.ndarray:
-        """
-        Computes the values of (s_xz, s_yz), the shear stresses
-
-        It's caused by two phenomenums: torsion and shear loading
-
-        We suppose that all points are inside the shape
-        """
-        values = np.zeros(points.shape, dtype="float64")
-        if self.forces[0] or self.forces[1]:  # Shear force
-            raise NotImplementedError
-        if self.moments[2]:  # Torsion moment
-            raise NotImplementedError
-        return values
