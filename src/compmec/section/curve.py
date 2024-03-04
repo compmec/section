46a2a80b4dc55721e10b16e79d2f10fb5b6139ed
carlos-adir <carlos.adir.leite@gmail.com>
2024-03-04 21:40:23 +0100

    refactor: divide Curve class in two
    separate the abstract interface (new ICurve) from the Curve class, which is responsible only of naming and tracking

--- src/compmec/section/curve.py
+++ src/compmec/section/curve.py
@@ -81,14 +81,71 @@ class Node:
         return tuple(Node.instances[label] for label in labels)
 
 
-class Curve(ABC):
+class ICurve(ABC):
     """
-    Abstract curve to be parent of others.
+    Interface abstract curve to be parent of others.
 
     This class serves as interface between the curves from others packaged
     like nurbs.Curve, to this package, expliciting the minimum requirements
     of a curve must have.
-    With this, it's possible to implement your only type of parametric curve
+    With this, it's possible to implement your own type of parametric curve
+    """
+
+    @classmethod
+    @abstractmethod
+    def from_dict(cls, dictionary: Dict) -> Curve:
+        """
+        Gives a curve instance based on the given parameters
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def to_dict(self) -> Dict:
+        """
+        Transforms a curve instance into a dictionary
+        """
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def limits(self) -> Tuple[float]:
+        """
+        Gives the curve's parametric interval
+
+        :getter: Returns the pair [a, b] in which curve is parametric defined
+        :type: Tuple[float]
+
+        """
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def knots(self) -> Tuple[float]:
+        """
+        Gives the curve's knots, in which the parametric interval is divided
+
+        :getter: Returns the knots that divides the curve's interval
+        :type: Tuple[float]
+
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def eval(self, parameters: Tuple[float]) -> Tuple[Tuple[float]]:
+        """
+        Evaluates the curves at given parameters.
+
+        :param parameters: A vector-like of lenght n
+        :type parameters: Tuple[float]
+        :return: A matrix of shape (n, 2)
+        :rtype: Tuple[Tuple[float]]
+        """
+        raise NotImplementedError
+
+
+class Curve(ICurve):
+    """
+    Base class that tracks the instances
     """
 
     instances = OrderedDict()
@@ -139,21 +196,6 @@ class Curve(ABC):
         Curve.instances[label] = instance
         return instance
 
-    @classmethod
-    @abstractmethod
-    def from_dict(cls, dictionary: Dict) -> Curve:
-        """
-        Gives a curve instance based on the given parameters
-        """
-        raise NotImplementedError
-
-    @abstractmethod
-    def to_dict(self) -> Dict:
-        """
-        Transforms a curve instance into a dictionary
-        """
-        raise NotImplementedError
-
     @property
     def label(self) -> int:
         """
@@ -183,42 +225,6 @@ class Curve(ABC):
             msg += f"New: '{self}'"
             raise ValueError(msg)
         self.instances[new_label] = self.instances.pop(cur_label)
-
-    @property
-    @abstractmethod
-    def limits(self) -> Tuple[float]:
-        """
-        Gives the curve's parametric interval
-
-        :getter: Returns the pair [a, b] in which curve is parametric defined
-        :type: Tuple[float]
-
-        """
-        raise NotImplementedError
-
-    @property
-    @abstractmethod
-    def knots(self) -> Tuple[float]:
-        """
-        Gives the curve's knots, in which the parametric interval is divided
-
-        :getter: Returns the knots that divides the curve's interval
-        :type: Tuple[float]
-
-        """
-        raise NotImplementedError
-
-    @abstractmethod
-    def eval(self, parameters: Tuple[float]) -> Tuple[Tuple[float]]:
-        """
-        Evaluates the curves at given parameters.
-
-        :param parameters: A vector-like of lenght n
-        :type parameters: Tuple[float]
-        :return: A matrix of shape (n, 2)
-        :rtype: Tuple[Tuple[float]]
-        """
-        raise NotImplementedError
 
 
 class NurbsCurve(Curve):
