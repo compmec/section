431ad79d160e2b171034157f81ac2f915571dd29
carlos-adir <carlos.adir.leite@gmail.com>
2024-03-05 20:38:14 +0100

    feat: increase version to 0.4.0

--- src/compmec/section/__init__.py
+++ src/compmec/section/__init__.py
@@ -3,10 +3,13 @@ This module analyse sections of beams by using the boundary element method
 It uses mainly curves as boundary to compute the elements.
 """
 
+from .curve import Curve
+from .dataio import JsonIO
+from .geometry import Geometry
 from .material import Isotropic, Material
 from .section import Section
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 if __name__ == "__main__":
     pass
