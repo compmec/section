fix: torsion vector computation

src/compmec/section/bem2d.py
@@ -127,7 +127,7 @@ class TorsionEvaluator:
         """
         Computes the matrix used to compute the torsion center
 
-                            [1] 
+                            [1]
         B = int_{Omega} w * [x] dOmega
                             [y]
 
@@ -147,7 +147,7 @@ class TorsionEvaluator:
         Computes the vector used to compute the torsion constant
 
         It's interested to compute J:
-        
+
         J = int_{tmin}^{tmax} w * <p, p'> dt
 
         Where w(t) is the warping function on the boundary of the curve.
@@ -171,23 +171,25 @@ class TorsionEvaluator:
         betas = np.einsum("ij,ij->i", vectors, vectors)
 
         cknots = np.array(self.curve.knots, dtype="float64")
-        tknots = np.array(sorted(set(self.curve.knots) | set(self.basis.knots)))
-        nodes, weights = Integration.opened(3)
-        
+        tknots = np.array(
+            sorted(set(self.curve.knots) | set(self.basis.knots))
+        )
+        nodes, weights = Integration.closed(2)
+
         for i, (alpha, beta) in enumerate(zip(alphas, betas)):
             tva, tvb = cknots[i], cknots[i + 1]
-            diff = tvb - tva
             mask = (tva <= tknots) * (tknots <= tvb)
             tmesh = tknots[mask]
             for tk0, tk1 in zip(tmesh, tmesh[1:]):
-                tvals = tk0 + nodes * (tk1 - tk0)
+                diff = tk1 - tk0
+                tvals = tk0 + nodes * diff
+                zvals = (tvals - tva) / (tvb - tva)
                 phis = self.basis.eval(tvals)
-                phis *= (tk1 - tk0) / (tvb - tva)
-                result += diff * alpha * np.einsum("i,ji->j", weights, phis)
-                result += diff * beta * np.einsum("i,i,ji->j", weights, tvals, phis)
+                result += diff * alpha * np.einsum("ij,j->i", phis, weights)
+                result += (
+                    diff * beta * np.einsum("ij,j,j->i", phis, weights, zvals)
+                )
         return result
-
-
 
 
 class ShearVector:

tests/test_bem2d.py
@@ -189,7 +189,6 @@ class TestComputeMatrix:
         pass
 
 
-
 class TestTorsionVectors:
     @pytest.mark.order(8)
     @pytest.mark.dependency(depends=["test_begin"])
@@ -198,11 +197,11 @@ class TestTorsionVectors:
 
     @pytest.mark.order(8)
     @pytest.mark.timeout(10)
-    @pytest.mark.dependency(depends=["TestComputeMatrix::test_begin"])
+    @pytest.mark.dependency(depends=["TestTorsionVectors::test_begin"])
     def test_square1_tensor_const_vector(self):
         vertices = [(0, 0), (1, 0), (1, 1), (0, 1)]
         vertices = np.array(vertices, dtype="float64")
-        good_vector = np.array([0, 1/2, 0, -1/2])
+        good_vector = np.array([0, 1 / 2, 0, -1 / 2])
 
         curve = PolygonCurve(vertices)
         basis = BasisFunc.cyclic(curve.knots)
@@ -212,10 +211,9 @@ class TestTorsionVectors:
         assert len(test_vector) == len(good_vector)
         np.testing.assert_allclose(test_vector, good_vector)
 
-    
     @pytest.mark.order(8)
     @pytest.mark.timeout(10)
-    @pytest.mark.dependency(depends=["TestComputeMatrix::test_begin"])
+    @pytest.mark.dependency(depends=["TestTorsionVectors::test_begin"])
     def test_square2_tensor_const_vector(self):
         vertices = [(-1, -1), (1, -1), (1, 1), (-1, 1)]
         vertices = np.array(vertices, dtype="float64")
@@ -231,7 +229,7 @@ class TestTorsionVectors:
 
     @pytest.mark.order(8)
     @pytest.mark.timeout(10)
-    @pytest.mark.dependency(depends=["TestComputeMatrix::test_begin"])
+    @pytest.mark.dependency(depends=["TestTorsionVectors::test_begin"])
     def test_square3_tensor_const_vector(self):
         vertices = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         vertices = np.array(vertices, dtype="float64")
@@ -245,14 +243,19 @@ class TestTorsionVectors:
         assert len(test_vector) == len(good_vector)
         np.testing.assert_allclose(test_vector, good_vector)
 
-
     @pytest.mark.order(8)
     @pytest.mark.timeout(10)
-    @pytest.mark.dependency(depends=["TestComputeMatrix::test_begin"])
-    def test_square3_tensor_const_vector(self):
-        vertices = [(3, 2), (2, 3), (1, 2), (2, 1)]
+    @pytest.mark.dependency(depends=["TestTorsionVectors::test_begin"])
+    def test_square4_tensor_const_vector(self):
+        a, b = 7, 13
+        vertices = [
+            (a - 1, b - 1),
+            (a + 1, b - 1),
+            (a + 1, b + 1),
+            (a - 1, b + 1),
+        ]
         vertices = np.array(vertices, dtype="float64")
-        good_vector = np.array([0, 0, 0, 0])
+        good_vector = np.array([a - b, a + b, b - a, -b - a])
 
         curve = PolygonCurve(vertices)
         basis = BasisFunc.cyclic(curve.knots)
@@ -262,14 +265,13 @@ class TestTorsionVectors:
         assert len(test_vector) == len(good_vector)
         np.testing.assert_allclose(test_vector, good_vector)
 
-
-
     @pytest.mark.order(8)
     @pytest.mark.dependency(
         depends=[
-            "TestComputeMatrix::test_square1_tensor_const_vector",
-            "TestComputeMatrix::test_square2_tensor_const_vector",
-            "TestComputeMatrix::test_square3_tensor_const_vector",
+            "TestTorsionVectors::test_square1_tensor_const_vector",
+            "TestTorsionVectors::test_square2_tensor_const_vector",
+            "TestTorsionVectors::test_square3_tensor_const_vector",
+            "TestTorsionVectors::test_square4_tensor_const_vector",
         ]
     )
     def test_end(self):
@@ -277,6 +279,8 @@ class TestTorsionVectors:
 
 
 @pytest.mark.order(8)
-@pytest.mark.dependency(depends=["TestComputeMatrix::test_end"])
+@pytest.mark.dependency(
+    depends=["TestComputeMatrix::test_end", "TestTorsionVectors::test_end"]
+)
 def test_end():
     pass