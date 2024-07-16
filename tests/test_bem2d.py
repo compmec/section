d9d988ff8240ade8af5aef4e975f8eb4ab67c35d
Carlos Adir <carlos.adir.leite@gmail.com>
2024-07-16 22:07:56 +0200

    test: add test for outcurve evaluation

--- tests/test_bem2d.py
+++ tests/test_bem2d.py
@@ -28,7 +28,19 @@ def test_begin():
     pass
 
 
-class TestComputeMatrix:
+class TestComputeMatrixIncurve:
+    """
+    Computes the integrals
+
+    int_{tmin}^{tmax} phi_j(t) * (r x p')/<r, r> dt
+
+    when
+
+    r = p(t) - s = p(t) - p(u)
+
+    s is a point on the curve
+    """
+
     @pytest.mark.order(8)
     @pytest.mark.dependency(depends=["test_begin"])
     def test_begin(self):
@@ -36,7 +48,7 @@ class TestComputeMatrix:
 
     @pytest.mark.order(8)
     @pytest.mark.timeout(10)
-    @pytest.mark.dependency(depends=["TestComputeMatrix::test_begin"])
+    @pytest.mark.dependency(depends=["TestComputeMatrixIncurve::test_begin"])
     def test_eqtriangle_corner(self):
         good_matrix = [[0, 1, 1], [1, 0, 1], [1, 1, 0]]
         good_matrix = np.array(good_matrix) / 12
@@ -53,7 +65,7 @@ class TestComputeMatrix:
 
     @pytest.mark.order(8)
     @pytest.mark.timeout(10)
-    @pytest.mark.dependency(depends=["TestComputeMatrix::test_begin"])
+    @pytest.mark.dependency(depends=["TestComputeMatrixIncurve::test_begin"])
     def test_eqtriangle_middle(self):
         pi, ln3, sq3 = np.pi, np.log(3), np.sqrt(3)
         good_matrix = [[-1, -1, 2], [2, -1, -1], [-1, 2, -1]]
@@ -72,7 +84,7 @@ class TestComputeMatrix:
 
     @pytest.mark.order(8)
     @pytest.mark.timeout(10)
-    @pytest.mark.dependency(depends=["TestComputeMatrix::test_begin"])
+    @pytest.mark.dependency(depends=["TestComputeMatrixIncurve::test_begin"])
     def test_square_corner(self):
         a = 1 / 8 - np.log(2) / (4 * np.pi)
         b = np.log(2) / (2 * np.pi)
@@ -90,7 +102,7 @@ class TestComputeMatrix:
 
     @pytest.mark.order(8)
     @pytest.mark.timeout(10)
-    @pytest.mark.dependency(depends=["TestComputeMatrix::test_begin"])
+    @pytest.mark.dependency(depends=["TestComputeMatrixIncurve::test_begin"])
     def test_square_middle(self):
         a = np.arctan(2) / (2 * np.pi) - np.log(5) / (8 * np.pi)
         b = 1 / 4 + np.log(5) / (8 * np.pi) - np.arctan(2) / (2 * np.pi)
@@ -108,7 +120,7 @@ class TestComputeMatrix:
 
     @pytest.mark.order(8)
     @pytest.mark.timeout(10)
-    @pytest.mark.dependency(depends=["TestComputeMatrix::test_begin"])
+    @pytest.mark.dependency(depends=["TestComputeMatrixIncurve::test_begin"])
     def test_hexagon_corner(self):
         pi, ln2, ln3, sq3 = np.pi, np.log(2), np.log(3), np.sqrt(3)
         a = 1 / 8 - sq3 * ln3 / (8 * pi)
@@ -136,7 +148,7 @@ class TestComputeMatrix:
 
     @pytest.mark.order(8)
     @pytest.mark.timeout(10)
-    @pytest.mark.dependency(depends=["TestComputeMatrix::test_begin"])
+    @pytest.mark.dependency(depends=["TestComputeMatrixIncurve::test_begin"])
     def test_hexagon_middle(self):
         pi, ln7, ln13, sq3 = np.pi, np.log(7), np.log(13), np.sqrt(3)
         a = -5 / 48 - sq3 * ln7 / (16 * pi) + 5 * np.arctan(5 / sq3) / (8 * pi)
@@ -171,12 +183,100 @@ class TestComputeMatrix:
     @pytest.mark.order(8)
     @pytest.mark.dependency(
         depends=[
-            "TestComputeMatrix::test_eqtriangle_corner",
-            "TestComputeMatrix::test_eqtriangle_middle",
-            "TestComputeMatrix::test_square_corner",
-            "TestComputeMatrix::test_square_middle",
-            "TestComputeMatrix::test_hexagon_corner",
-            "TestComputeMatrix::test_hexagon_middle",
+            "TestComputeMatrixIncurve::test_eqtriangle_corner",
+            "TestComputeMatrixIncurve::test_eqtriangle_middle",
+            "TestComputeMatrixIncurve::test_square_corner",
+            "TestComputeMatrixIncurve::test_square_middle",
+            "TestComputeMatrixIncurve::test_hexagon_corner",
+            "TestComputeMatrixIncurve::test_hexagon_middle",
+        ]
+    )
+    def test_end(self):
+        pass
+
+
+class TestComputeMatrixOutcurve:
+    """
+    Test the values of the integrals:
+
+    int_{Gamma}
+    """
+
+    @pytest.mark.order(8)
+    @pytest.mark.dependency(depends=["test_begin"])
+    def test_begin(self):
+        pass
+
+    @pytest.mark.order(8)
+    @pytest.mark.timeout(10)
+    @pytest.mark.dependency(depends=["TestComputeMatrixOutcurve::test_begin"])
+    def test_square(self):
+        ln3, ln5, ln13 = np.log(3), np.log(5), np.log(13)
+        atan = np.arctan
+        atan2, atan3 = np.arctan(2), np.arctan(3)
+        good_matrix = np.zeros((3, 4), dtype="float64")
+        good_matrix[0, :] = np.pi / 2
+        good_matrix[1, 0] = (
+            -ln5 / 4
+            + atan(1 / 2) / 4
+            + atan(3 / 2) / 4
+            + atan(2 / 3)
+            + ln13 / 4
+        )
+        good_matrix[1, 1] = (
+            -ln13 / 4
+            + 3 * atan(1 / 2) / 4
+            + ln5 / 4
+            + 3 * atan(3 / 2) / 4
+            + atan(2)
+        )
+        good_matrix[1, 2] = (
+            -ln13 / 4
+            + 3 * atan(1 / 2) / 4
+            + ln5 / 4
+            + 3 * atan(3 / 2) / 4
+            + atan2
+        )
+        good_matrix[1, 3] = (
+            -ln5 / 4
+            + atan(1 / 2) / 4
+            + atan(3 / 2) / 4
+            + atan(2 / 3)
+            + ln13 / 4
+        )
+        good_matrix[2, 0] = (
+            -3 * ln5 / 4 + atan(1 / 3) / 2 + np.pi / 8 + 3 * ln3 / 2
+        )
+        good_matrix[2, 1] = (
+            -3 * ln3 / 4
+            + 3 * atan(1 / 3) / 4
+            + atan(3) / 4
+            + np.pi / 4
+            + ln5 / 2
+        )
+        good_matrix[2, 2] = -ln5 / 4 + 3 * np.pi / 8 + 3 * atan(3) / 2
+        good_matrix[2, 3] = (
+            -3 * ln3 / 4
+            + 3 * atan(1 / 3) / 4
+            + atan3 / 4
+            + np.pi / 4
+            + ln5 / 2
+        )
+
+        vertices = [(-2, -2), (2, -2), (2, 2), (-2, 2)]
+        sources = [(0, 0), (1, 0), (1, 1)]
+        curve = Curve.from_vertices(vertices)
+        basis = BasisFunc.cyclic(curve.knots)
+        test_matrix = ComputeStiffness.outcurve(curve, basis, sources)
+
+        assert test_matrix.shape == good_matrix.shape
+        np.testing.assert_allclose(test_matrix, good_matrix)
+
+    @pytest.mark.order(8)
+    @pytest.mark.dependency(
+        depends=[
+            "TestComputeMatrixOutcurve::test_begin",
+            "TestComputeMatrixOutcurve::test_square",
         ]
     )
     def test_end(self):
@@ -270,7 +370,11 @@ class TestTorsionVectors:
 
 @pytest.mark.order(8)
 @pytest.mark.dependency(
-    depends=["TestComputeMatrix::test_end", "TestTorsionVectors::test_end"]
+    depends=[
+        "TestComputeMatrixIncurve::test_end",
+        "TestComputeMatrixOutcurve::test_end",
+        "TestTorsionVectors::test_end",
+    ]
 )
 def test_end():
     pass
